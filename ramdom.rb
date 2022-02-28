b1fbaf16f07844a8fce3cb3d04a5c5065e270d58
yamaryo416 <rys0707@gmail.com>
2022-02-28 17:53:38 +0900

    feat: 配列をランダムに分けるスクリプト作成

---
+++ ramdom.rb
@@ -0,0 +1,14 @@
+# 順序がバラバラなユーザーの配列
+users = %w[A B C D E F].shuffle
+
+# ランダムに2:4か3:3に切るための変数
+slice_position = [1, 2].sample
+
+# 二つのgroupに分けて、配列に格納
+groups = users.partition.with_index { |_, index| index <= slice_position }
+
+
+groups.each do |group|
+  ## それぞれのgroupをsortして表示
+  pp group.sort
+end
